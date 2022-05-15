13b8a691d946180aadcdb81c0617b45908de0303
Lázaro <lazaronobrega3@gmail.com>
2022-05-15 06:47:24 -0300

    Show score

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,17 +1,22 @@
 
 import {ReactComponent as Arrow} from 'assets/img/arrow.svg';
+import { MoviePage } from 'types/movie';
 
-function Pagination(){
+type Props = {
+    page: MoviePage;
+}
+
+function Pagination({page}: Props){
     
     return(
 
     <div className="dsmovie-pagination-container">
         <div className="dsmovie-pagination-box">
-            <button className="dsmovie-pagination-button" disabled={true} >
+            <button className="dsmovie-pagination-button" disabled={page.first} >
                 <Arrow />
             </button>
-            <p>{`${1} de ${3}`}</p>
-            <button className="dsmovie-pagination-button" disabled={false} >
+            <p>{`${page.number} de ${page.totalPages}`}</p>
+            <button className="dsmovie-pagination-button" disabled={page.last} >
                 <Arrow className="dsmovie-flip-horizontal" />
             </button>
         </div>
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -26,7 +26,7 @@ function Listing(){
 
     useEffect(() => {
 
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
         .then(response =>{
             const data = response.data as MoviePage;
             setPage(data);
@@ -37,7 +37,7 @@ function Listing(){
     return(
         <>
 
-        <Pagination />
+        <Pagination page={page}/>
 
             <div className="container">
                 <div className="row">
